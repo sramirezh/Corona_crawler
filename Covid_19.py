b44a8c7d9bcd0cf3d4e56dc29a2d6c4b12062adb
MAC_simon <sramirez.hinestrosa@gmail.com>
2020-03-28 12:57:11 +0100

    Before paralellising

--- Covid_19.py
+++ Covid_19.py
@@ -22,42 +22,64 @@ https://chercher.tech/python/table-selenium-python
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options 
+from tqdm import tqdm
+from joblib import Parallel, delayed
+import pandas as pd
+import multiprocessing
+
+
 
 
 class Coronavirus():
   def __init__(self):
-    self.driver = webdriver.Chrome()
+    self.driver = webdriver.Chrome(chrome_options = chrome_options)
     self.driver.get('https://www.worldometers.info/coronavirus/')
+
+# To run it headless
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+
+
     
-    
+
 bot = Coronavirus()
 
-table = bot.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]')
+table = bot.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]') # There are 2 table bodies
+header = bot.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/thead')
 
 #country_element = table.find_element_by_xpath("//tr[contains(text(), 'USA')]")
     
 class WebTable:
-    def __init__(self, webtable):
-       self.table = webtable
-       self.row_number = self.row_count()
-       self.column_number =  self.column_count()
+    def __init__(self, webtable, data_type = "d"):
+        """
+        The type is d = data or h for header
+        """
+        self.type = data_type
+        self.table = webtable
+        self.row_number = self.get_row_count()
+        self.column_number =  self.get_column_count()
 
     def get_row_count(self):
-      return len(self.table.find_elements_by_tag_name("tr")) - 1
+      return len(self.table.find_elements_by_tag_name("tr"))
 
     def get_column_count(self):
-        return int(len(self.table.find_elements_by_xpath("//tr[2]/td"))/2)
+        if self.type == 'h':
+            return int(len(self.table.find_elements_by_xpath("//tr/t%s"%self.type))/2)
+        
+        if self.type == 'd':
+            return int(len(self.table.find_elements_by_xpath("//tr[1]/t%s"%self.type))/2) 
 
     def get_table_size(self):
         return {"rows": self.row_number,
                 "columns": self.column_number}
 
     def row_data(self, row_number):
-        if(row_number == 0):
-            raise Exception("Row number starts from 1")
-
-        row_number = row_number + 1
-        row = self.table.find_elements_by_xpath("//tr["+str(row_number)+"]/td")
+        """
+        Important in HTML apparently the count starts in 1 not in 0
+        """
+        row_number = row_number
+        row = self.table.find_elements_by_xpath("//tr["+str(row_number)+"]/t%s"%self.type)
         rData = []
         for webElement in row :
             rData.append(webElement.text)
@@ -65,7 +87,7 @@ class WebTable:
         return rData
 
     def column_data(self, column_number):
-        col = self.table.find_elements_by_xpath("//tr/td["+str(column_number)+"]")
+        col = self.table.find_elements_by_xpath("//tr/t%s["%self.type+str(column_number)+"]")
         rData = []
         for webElement in col :
             rData.append(webElement.text)
@@ -73,56 +95,29 @@ class WebTable:
 
     def get_all_data(self):
         # get number of rows
-        noOfRows = len(self.table.find_elements_by_xpath("//tr")) -1
-        # get number of columns
-        noOfColumns = int(len(self.table.find_elements_by_xpath("//tr[2]/td"))/2)
         allData = []
         # iterate over the rows, to ignore the headers we have started the i with '1'
-        for i in range(2, noOfRows):
+        num_cores = multiprocessing.cpu_count()
+        msd=Parallel(n_jobs=num_cores)(delayed(compute_one_msd)(pos,i+1,"single") for i in tqdm(range(max_delta)))    
+        for i in tqdm(range(self.row_number)):
             # reset the row data every time
-            ro = []
-            # iterate over columns
-            for j in range(1, noOfColumns) :
-                # get text from the i th row and j th column
-                ro.append(self.table.find_element_by_xpath("//tr["+str(i)+"]/td["+str(j)+"]").text)
-
-            # add the row data to allData of the self.table
-            allData.append(ro)
-
+            row = self.row_data(i+1)
+            allData.append(row)
+            
         return allData
 
-    def presence_of_data(self, data):
-
-        # verify the data by getting the size of the element matches based on the text/data passed
-        dataSize = len(self.table.find_elements_by_xpath("//td[normalize-space(text())='"+data+"']"))
-        presence = false
-        if(dataSize > 0):
-            presence = true
-        return presence
-
-    def get_cell_data(self, row_number, column_number):
-        if(rowNumber == 0):
-            raise Exception("Row number starts from 1")
-
-        rowNumber = rowNumber+1
-        cellData = table.find_element_by_xpath("//tr["+str(row_number)+"]/td["+str(column_number)+"]").text
-        return cellData
     
     
     
-w = WebTable(table)
+w = WebTable(table,"d")
+h = WebTable(header,"h")
+
 print("No of rows : ", w.get_row_count())
 print("------------------------------------")
 print("No of cols : ", w.get_column_count())
 print("------------------------------------")
 print("Table size : ", w.get_table_size())
-print("------------------------------------")
-print("First row data : ", w.row_data(1))
-print("------------------------------------")
-print("First column data : ", w.column_data(1))
-print("------------------------------------")
-print("All table data : ", w.get_all_data())
-print("------------------------------------")
-print("presence of data : ", w.presence_of_data("Chercher.tech"))
-print("------------------------------------")
-print("Get data from Cell : ", w.get_cell_data(2, 2))
+
+
+data = w.get_all_data()
+header = h.get_all_data()
